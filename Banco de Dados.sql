e69d80aadc2884362e4c84e07bef39580a80f393
Victor Augusto Moraes Pereira <111441786+VictorXdAugusto@users.noreply.github.com>
2022-09-07 10:34:52 -0300

    Update Banco de Dados.sql

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -2,8 +2,8 @@ create database Coffee_Health;
 use Coffee_Health;
 
 -- CRIANDO A TABELA DE CADASTRO
-create table cadastro(
-id int primary key auto_increment,
+create table Cadastro(
+idCadastro int primary key auto_increment,
 email varchar(60),
 senha varchar(20),
 nome varchar(100),
@@ -11,8 +11,8 @@ constraint chkemail check (email like '%@%')
 );
 
 -- CRIANDO TABELA PLANTAÇÃO
-create table plantacao(
-id int primary key auto_increment,
+create table Plantacao(
+idPlantacao int primary key auto_increment,
 especie varchar(15),
 estado char(2),
 cidade varchar(30),
@@ -23,27 +23,27 @@ constraint chkestado check (estado in ('AC','AL','AP','AM','BA','CE','ES','GO','
 );
 
 -- CRIANDO TABELA CONTROLE
-create table controle(
-id int primary key auto_increment,
+create table Controle(
+idControle int primary key auto_increment,
 temperatura double,
 umidade float,
 momento datetime default current_timestamp
 );
 
 -- INSERINDO DADOS
-insert into cadastro (email, senha, nome) values
+insert into Cadastro (email, senha, nome) values
 ('diego.vieira@sptech.school','endiegoagronomo','Diego Vieira'),
 ('lucas.bsilva@sptech.school','lucas123','Lucas Bonfim'),
 ('estela.polverini@sptech.school','seeyoulater','Estela Polverini'),
-('victor.pereira@sptech.school','12345678','Victor Augusto');
+('victor.pereira@sptech.school','eovitao','Victor Augusto');
 
-insert into plantacao (especie, estado, cidade, medida, tamanho) values
+insert into Plantacao (especie, estado, cidade, medida, tamanho) values
 ('Arábica','MG','Carmo de Minas','ha',2),
 ('Robusta','MT','Juína','m²',3),
 ('Eugenioides','SP','Joanópolis','m²',10),
 ('Arábica','MS','Eldorado','ha',1);
 
-insert into controle (temperatura, umidade) values
+insert into Controle (temperatura, umidade) values
 (15.9,50.0),
 (15.8,50.0),
 (15.8,50.0),
